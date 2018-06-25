4494d4ddf4bf228a0b09d2f477761d0ab14d7515
Ocean <dvsocean@icloud.com>
2018-06-25 15:40:08 -0700

    creditcard microservice build

---
+++ src/main/java/com/retailbank/creditcardservice/CreditcardserviceApplication.java
@@ -0,0 +1,12 @@
+package com.retailbank.creditcardservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditcardserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditcardserviceApplication.class, args);
+	}
+}
