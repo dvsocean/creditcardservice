244a1a6c9a221b8428a50047320464b491954a1c
Ocean <dvsocean@icloud.com>
2018-06-27 15:48:24 -0700

    done

--- src/main/java/com/retailbank/creditcardservice/CreditCardApplicationsController.java
+++ src/main/java/com/retailbank/creditcardservice/CreditCardApplicationsController.java
@@ -1,23 +1,29 @@
 package com.retailbank.creditcardservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 public class CreditCardApplicationsController {
 
   private final RestTemplate restTemplate;
+  private final String creditcheckserviceBaseUrl;
 
-  public CreditCardApplicationsController(RestTemplate restTemplate) {
+  public CreditCardApplicationsController(RestTemplate restTemplate, @Value("${creditcheckservice.baseUrl}") String creditcheckserviceBaseUrl) {
     this.restTemplate = restTemplate;
+    this.creditcheckserviceBaseUrl = creditcheckserviceBaseUrl;
   }
 
   @PostMapping("/credit-card-applications")
   public ApplyForCreditCardResponse applyForCreditCard(@RequestBody final ApplyForCeditCardRequest applyForCeditCardRequest){
     final int citizenNumber = applyForCeditCardRequest.getCitizenNumber();
-    final CreditCheckResponse creditCheckResponse = restTemplate.postForObject("http://localhost:8080/credit-scores", new CreditCheckRequest(citizenNumber), CreditCheckResponse.class);
+    //defined in application.properties file
+    final String url = UriComponentsBuilder.fromHttpUrl(creditcheckserviceBaseUrl).path("credit-scores").toUriString();
+    final CreditCheckResponse creditCheckResponse = restTemplate.postForObject(url, new CreditCheckRequest(citizenNumber), CreditCheckResponse.class);
     if(creditCheckResponse.getScore() == Score.HIGH && applyForCeditCardRequest.getCardType() == CardType.GOLD){
       return new ApplyForCreditCardResponse(Status.GRANTED);
     }
